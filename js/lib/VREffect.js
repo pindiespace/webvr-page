096788b31b87d788e7761fe019112610e13c515f
Dr. Pete Markiewicz <pindiespace@gmail.com>
2015-12-31 11:14:18 -0800

    Updated locally
    local update

--- js/lib/VREffect.js
+++ js/lib/VREffect.js
@@ -74,12 +74,6 @@ THREE.VREffect = function ( renderer, onError ) {
 
 	};
 
-	//NOTE: added
-	this.setFOV = function(fovL, fovR) {
-		eyeFOVL = fovL;
-		eyeFOVR = fovR;
-	};
-
 	// fullscreen
 
 	var isFullscreen = false;
